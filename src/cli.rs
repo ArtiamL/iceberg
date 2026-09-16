d1f058f0f0d394fc777bbb881c2d32346c6d3efd
ArtiamL <main.lnewell@gmail.com>
2026-09-16 16:42:36 +0100

    Added help info to all Commands and Args. Renamed deserialise_from_file() & serialize_to_file() to load() and save() respectively.

--- src/cli.rs
+++ src/cli.rs
@@ -4,10 +4,14 @@ pub use clap::{Parser, Subcommand};
 
 #[derive(Parser, Debug)]
 #[command(
-    name = "iceberg",
+    name = "Iceberg",
     version = "0.1.0", 
-    about = "That's just the tip of the iceberg!",
-    long_about = None)]
+    about = "That's just the tip of the Iceberg!",
+    long_about = None,
+    override_usage = 
+        "iceberg                      # Launches the interactive dashboard
+       iceberg [COMMAND] <args>     # Run a single action"
+)]
 pub struct Cli {
     #[command(subcommand)]
     pub command: Option<Commands>,
@@ -15,27 +19,40 @@ pub struct Cli {
 
 #[derive(Subcommand, Debug)]
 pub enum Commands {
+    #[command(about = "List all Iceberg entries")]
+    List,
+    #[command(about = "Add an entry to the Iceberg")]
     Add {
+        #[arg(help = "The title of the entry. This field is required")]
         title: String,
 
-        #[arg(short, long, help = "requires information (\"\")")]
+        #[arg(short, long, help = "Optional: If given, add information (\"\")")]
         info: Option<String>,
         // TODO:
         // #[arg(short, long)]
         // date: Option<T>,
         //
     },
-    #[command(visible_alias = "rm")]
+    #[command(visible_alias = "rm", about = "Remove an entry from the Iceberg")]
     Remove {
+        #[arg(help = "The ID of the entry to remove")]
         id: Option<u32>,
-        #[arg(long)]
+        #[arg(
+            long,
+            help = "Removes \x1b[1;4mALL\x1b[22;24m entries in the Iceberg - prompts for confirmation"
+        )]
         all: bool,
-        #[arg(long, requires = "all")]
+        #[arg(
+            long,
+            requires = "all",
+            help = "\x1b[31mDanger:\x1b[0m Removes \x1b[1;4mALL\x1b[22;24m entries in the Iceberg \x1b[1;4mwithout\x1b[22;24m confirmation"
+        )]
         no_confirm: bool,
     },
-    List,
+    #[command(about = "Mark an Icerberg entry as complete")]
     Complete {
-        id: u32,
+        #[arg(help = "The ID of the entry to mark as complete")]
+        id: u32
     },
 }
 
@@ -45,7 +62,7 @@ pub enum CliError {
     AlreadyExists(String),
     EntryNotFound(u32),
     IoError(std::io::Error),
-    JsonError(serde_json::Error),
+    JsonParseError(serde_json::Error),
     ConfigDirNotFound,
     NoConfirmError,
     NoIdGivenError,
@@ -61,7 +78,7 @@ impl From<std::io::Error> for CliError {
 
 impl From<serde_json::Error> for CliError {
     fn from(err: serde_json::Error) -> Self {
-        Self::JsonError(err)
+        Self::JsonParseError(err)
     }
 }
 
@@ -77,7 +94,7 @@ impl Display for CliError {
             }
             Self::IoError(err) => write!(f, "I/O Error: {err}"),
             Self::ConfigDirNotFound => write!(f, "The config directory was not found!"),
-            Self::JsonError(err) => write!(f, "Json conversion error: {err}"),
+            Self::JsonParseError(err) => write!(f, "Error parsing JSON: {err}"),
             Self::NoConfirmError => write!(f, "--no-confirm must be used with --all"),
             Self::NoIdGivenError => write!(f, "No ID was given! to remove all entries use --all"),
         }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use std::{
-    fmt::format,
     fs::{self},
     io::{self, Write},
     process,
@@ -20,7 +19,7 @@ fn main() {
 }
 
 fn run() -> Result<(), CliError> {
-    let mut entries: Vec<Entry> = deserialize_from_file().unwrap_or_default();
+    let mut entries: Vec<Entry> = load().unwrap_or_default();
     let next_id = entries.iter().map(|e| e.id).max().unwrap_or(0) + 1;
 
     let cli = Cli::parse();
@@ -42,7 +41,7 @@ fn run() -> Result<(), CliError> {
             println!("Added new item:");
             println!("{new_entry}");
             entries.push(new_entry);
-            serialize_to_file(&entries)
+            save(&entries)
         }
         Commands::Remove {
             id,
@@ -51,15 +50,14 @@ fn run() -> Result<(), CliError> {
         } => {
             match (id, all, no_confirm) {
                 (Some(id), _, _) => {
-                    // id given, ignore everything else (remove only entry
-                    // with id)
+                    // id given, ignore everything else (remove only entry with id)
                     let Some(entry) = entries.iter().position(|e| e.id == id) else {
                         return Err(CliError::EntryNotFound(id));
                     };
                     let removed = entries.remove(entry); // Decide if elem order is needed - if not use
                     // swap_remove
 
-                    serialize_to_file(&entries)?;
+                    save(&entries)?;
 
                     println!("Removed entry:\n{removed}");
                     Ok(())
@@ -83,7 +81,7 @@ fn run() -> Result<(), CliError> {
 
                     entries.clear();
 
-                    serialize_to_file(&entries)?;
+                    save(&entries)?;
                     println!("Cleared all entries!");
                     Ok(())
                 }
@@ -91,7 +89,7 @@ fn run() -> Result<(), CliError> {
                     // no id, --all given, --no-confirm given (remove everything)
                     entries.clear();
 
-                    serialize_to_file(&entries)?;
+                    save(&entries)?;
                     println!("Cleared all entries!");
                     Ok(())
                 }
@@ -104,7 +102,7 @@ fn run() -> Result<(), CliError> {
             entry.is_complete = true;
 
             let msg = format!("Marked entry as complete:\n{entry}");
-            serialize_to_file(&entries)?;
+            save(&entries)?;
 
             println!("{msg}");
             Ok(())
@@ -135,7 +133,7 @@ fn load_dashboard() {
     // }
 }
 
-fn serialize_to_file(entries: &[Entry]) -> Result<(), CliError> {
+fn save(entries: &[Entry]) -> Result<(), CliError> {
     let json = serde_json::to_string(&entries)?;
 
     let data_dir = dirs::data_local_dir()
@@ -149,7 +147,7 @@ fn serialize_to_file(entries: &[Entry]) -> Result<(), CliError> {
     Ok(())
 }
 
-fn deserialize_from_file() -> Result<Vec<Entry>, CliError> {
+fn load() -> Result<Vec<Entry>, CliError> {
     let data_dir = dirs::data_local_dir()
         .ok_or(CliError::ConfigDirNotFound)?
         .join("iceberg");
